119edcd28544e00aea4b3084991dfdc0473be765
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-18 20:47:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,9 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   
   name_hash.each do |key, value|
-    if lowe
+    if lowest_value.nil?
+      lowest_value = v
+      lowest_key = k
     if value < lowest_value
       lowest_value = v
       lowest_key = k
