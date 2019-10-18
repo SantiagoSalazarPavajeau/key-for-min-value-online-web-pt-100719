924c0369bccad205f7642c212df4af329738251f
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-18 20:37:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = Float::INFINITY
-  name_hash.each do |k, v|
-    if v < lowest_value
+  name_hash.each do |key, value|
+    if value < lowest_value
       lowest_value = v
       lowest_key = k
     end
